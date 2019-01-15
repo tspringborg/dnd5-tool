6d1fbc6280a847c194b61b447cd4539bf1ca13f2
Thomas Springborg <ts@Thomass-MacBook-Pro.local>
2019-01-15 14:56:33 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="container">
+        <div>
+            <h1 class="title">
+                dnd5-tool
+            </h1>
+            <h2 class="subtitle">
+                A tool for dnd5
+            </h2>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        },
+        methods: {
+        },
+    }
+</script>
