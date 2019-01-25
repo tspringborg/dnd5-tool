5839a04f177eaf3e6ffe34ecec89c0c401fe3cf6
Thomas Springborg <ts@Thomass-MacBook-Pro.local>
2019-01-25 16:01:59 +0100

    ok

--- pages/spells/index.vue
+++ pages/spells/index.vue
@@ -1,16 +1,13 @@
 <template>
     <div>
         <div>{{ list.length }}</div>
-        <table>
-            <th
-                v-for="(name, index) in ['name']"
-                :key="index"
-                @click="sortBy = name"
-            >
-                {{ name }}
-            </th>
-        </table>
-        <div v-for="(spell, index) in list" :key="index">
+        <VueGoodTable
+            :columns="tableColumns"
+            :rows="tableRows"
+        />
+        <div
+            v-for="(spell, index) in list"
+            :key="index">
             <nuxt-link
                 :to="`spells/${encodeURIComponent(spell.name)}`"
             >
@@ -23,9 +20,14 @@
 <script>
 
 import Fuse from 'fuse.js'
+import _ from 'lodash'
+import { VueGoodTable } from 'vue-good-table'
 
 export default {
     name: 'Spells',
+    components: {
+        VueGoodTable,
+    },
     data() {
         return {
             sortBy: null,
@@ -34,6 +36,39 @@ export default {
     computed: {
         list() {
             return this.$store.state.spells.list
+        },
+        tableColumns() {
+            return [
+                {
+                    label: 'Level',
+                    field: 'level',
+                    type: 'number',
+                },
+                {
+                    label: 'Name',
+                    field: 'name',
+                },
+                {
+                    label: 'Classes',
+                    field: 'classlist',
+                    type: 'array',
+                },
+                {
+                    label: 'Source',
+                    field: 'source',
+                },
+            ]
+        },
+        tableRows() {
+            return _.map(this.list, (item, index) => {
+                return {
+                    ...item,
+                    source: item.source + '(page '+item.page + ')',
+                    classlist: _.map(item.classes.fromClassList, (c) => {
+                        return c.name
+                    }),
+                }
+            })
         },
     },
     mounted() {
